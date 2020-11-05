6d8924484fae7cc0bf87bdc63f286b992e0903fe
winceos <winceos@gmail.com>
2020-11-04 16:53:08 -0800

    format Node parameters

--- nav2_bringup/bringup/launch/slam_launch.py
+++ nav2_bringup/bringup/launch/slam_launch.py
@@ -76,14 +76,15 @@ def generate_launch_description():
 
     start_map_saver_server_cmd = Node(
             package='nav2_map_server',
-            node_executable='map_saver_server',
+            executable='map_saver_server',
+            name='map_saver',
             output='screen',
             parameters=[configured_params])
 
     start_lifecycle_manager_cmd = Node(
             package='nav2_lifecycle_manager',
-            node_executable='lifecycle_manager',
-            node_name='lifecycle_manager_slam',
+            executable='lifecycle_manager',
+            name='lifecycle_manager_slam',
             output='screen',
             parameters=[{'use_sim_time': use_sim_time},
                         {'autostart': autostart},
